322cb1a9538160bf2c44b4ce86d1adc42a8ffc5b
신지훈 <rec8730@gmail.com>
2023-01-08 03:35:35 +0900

    게시판 수정기능 구현완료

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,9 +1,22 @@
 import React from "react";
 import "./nav.scss";
 import { useNavigate } from "react-router-dom";
+import { getAuth, signOut } from "firebase/auth";
 
 const Nav = () => {
   const goToHome = useNavigate();
+
+  const goToLogin = useNavigate();
+
+  const onLogoutClick = () => {
+    const auth = getAuth();
+    const isLogout = window.confirm("로그아웃 하시겠습니까");
+    if (isLogout) {
+      signOut(auth);
+      goToLogin("/");
+      window.location.reload();
+    }
+  };
 
   return (
     <div className="Nav">
@@ -16,6 +29,9 @@ const Nav = () => {
         Home
       </div>
       <h2>Shin Ji hoon</h2>
+      <div className="logout" onClick={onLogoutClick}>
+        로그아웃
+      </div>
     </div>
   );
 };
--- src/pages/Boarddetail/Boarddetail.js
+++ src/pages/Boarddetail/Boarddetail.js
@@ -1,34 +1,109 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import "./boarddetail.scss";
-import { deleteDoc, doc } from "firebase/firestore";
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
 import { db } from "../../firebase";
 
 const Boarddetail = ({ boardData, userObj }) => {
-  const goToBoard = useNavigate();
+  const [isUpdate, setIsUpdate] = useState(false);
+  const [newUpdateContents, setNewUpdateContents] = useState();
+  const [newUpdateTitle, setNewUpdateTitle] = useState();
+
   let { id } = useParams();
   let userId = boardData?.find((board) => {
     return board.id === id;
   });
+  console.log(userId?.id);
+  const goToBoard = useNavigate();
+
+  const toggleUpdate = () => setIsUpdate((prev) => !prev);
+
+  const onSubmit = async (event) => {
+    const isUpdateConfirm = window.confirm("정말로 게시글을 삭제하시겠습니까?");
+    const boardUser = doc(db, "econrich", `${userId?.id}`);
+    event.preventDefault();
+    if (isUpdateConfirm) {
+      await updateDoc(boardUser, {
+        postTitle: newUpdateTitle,
+        postContents: newUpdateContents,
+      });
+      setIsUpdate(false);
+    }
+  };
+
+  const onChangeTitle = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewUpdateTitle(value);
+  };
+
+  const onChangeContents = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewUpdateContents(value);
+  };
 
   const onDeleteStyle = () => {
-    const isDeleteConfirm = window.confirm("정말로 스타일을 삭제하시겠습니까?");
+    const isDeleteConfirm = window.confirm("게시글을 삭제하시겠습니까?");
     const boardDelete = doc(db, "econrich", `${userId?.id}`);
     if (isDeleteConfirm) {
       goToBoard("/board");
       deleteDoc(boardDelete);
     }
   };
+
   return (
     <>
       <div className="Boarddetail">
-        <div className="board-title">{userId?.postTitle}</div>
-        <div className="board-contents">{userId?.postContents}</div>
-        {userObj.uid === userId.createId ? (
-          <div className="board-delete" onClick={onDeleteStyle}>
-            삭제
-          </div>
-        ) : null}
+        {isUpdate ? (
+          <>
+            <form onSubmit={onSubmit}>
+              <input
+                className="title-input"
+                value={newUpdateTitle}
+                type="text"
+                placeholder="제목을 수정해 주세요"
+                maxLength={15}
+                onChange={onChangeTitle}
+              />
+              <textarea
+                className="contents-area"
+                value={newUpdateContents}
+                placeholder="본문을 수정해 주세요."
+                required
+                onChange={onChangeContents}
+                maxLength={150}
+              />
+              <input className="update" type="submit" value="수정하기" />
+            </form>
+            <button
+              className="delete"
+              onClick={() => {
+                setIsUpdate(false);
+              }}
+            >
+              취소
+            </button>
+          </>
+        ) : (
+          <>
+            <div className="board-title">{userId?.postTitle}</div>
+            <div className="board-contents">{userId?.postContents}</div>
+
+            {userObj?.uid === userId?.createId ? (
+              <>
+                <div className="board-delete" onClick={toggleUpdate}>
+                  수정
+                </div>
+                <div className="board-delete" onClick={onDeleteStyle}>
+                  삭제
+                </div>
+              </>
+            ) : null}
+          </>
+        )}
       </div>
     </>
   );
